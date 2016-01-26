73c2f6944ef042a9f13a1a2a86a14a1b4fad99f8
Steven Leiva <leiva.steven@gmail.com>
2016-01-26 12:05:45 -0500

    Allow a comment to output raw HTML.
    The API for this is purposely difficult. React wants you to have to go
    out of your way to do this.

--- public/scripts/personalExample.js
+++ public/scripts/personalExample.js
@@ -4,7 +4,25 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitiaze: true});
+    return {__html: rawMarkup};
+  },
+  render: function() {
+    return (
+      <div className="Comment">
+        <h2 className="Author">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
