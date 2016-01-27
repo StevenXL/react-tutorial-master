40295672879b9d82d97d2115d75dbb00ab6f4534
Steven Leiva <stevenleiva@Stevens-MacBook-Pro.local>
2016-01-27 08:46:02 -0500

    Update CommentForm component so that it is able to submit new comments to the server.
    There is a lot going on this commit. Let's go over them one by one.
    
    1. First, we have added an `getInitialState` function to our component
    in order to set the initial state of the component. Again, the name of
    the function is odd, since we use it to set the initial state. Remember
    that this will only be run once in the lifetime of a component.
    
    2. We added two functions - `handleAuthorChange`, `handleTextChange` -
    that make sure that when our user types into one of the input elements,
    that input is reflected in our component. We reflect it in the component
    by updating the component's state with `setState`. Notice that we are
    setting the `author` or `text` property of the `state` object - which
    lives on the component - to a particular value.
    
    3. We added a `handleSubmit` function whose job it is to send the data
    to the `CommentBox` component and to change reset the form by changing
    the state of the `CommentForm` component. The first part deserves some
    explanation. The only way that we are able to pass data from the child
    to the parent is by passing in a callback function that lives on the
    parent as a property of the child. In other words, the function
    `handleCommentSubmit` is defined on the CommentBox component. It is
    passed down as a property on the CommentForm component. By asking
    CommentForm to invoke this callback, we can effectively pass data to the
    parent CommentBox.

--- public/scripts/personalExample.js
+++ public/scripts/personalExample.js
@@ -41,16 +41,54 @@ var Comment = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+
+    if (!author || !text) {
+      return
+    }
+
+    this.props.onCommentSubmit({author: author, text: text});
+
+    this.setState({author: '', text: ''});
+  },
   render: function() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit} >
+        <input type="text" placeholder="Your Name..." value={this.state.author} onChange={this.handleAuthorChange} />
+        <input type="text" placeholder="Say Something..." value={this.state.text} onChange={this.handleTextChange} />
+        <input type="submit" placeholder="Post" />
+      </form>
     );
   }
 });
 
 var CommentBox = React.createClass({
+  handleCommentSubmit: function(comment) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   loadCommentsFromServer: function() {
     $.ajax({
       url: this.props.url,
@@ -76,7 +114,7 @@ var CommentBox = React.createClass({
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
